d6ea5ffcc3595ae2164d2bf87dbe21963ad87869
Te Vallee <astronomemusic@gmail.com>
2015-09-07 14:24:15 -1000

    edited karma config

--- karma.conf.js
+++ karma.conf.js
@@ -1,16 +1,62 @@
+'use strict';
 // Karma configuration
-// Generated on Mon Sep 07 2015 13:25:52 GMT-1000 (HST)
-var sharedConfig = require('./karma-shared.conf');
+// Generated on Mon Sep 07 2015 14:13:24 GMT-1000 (HST)
 
 module.exports = function(config) {
-  var conf = sharedConfig();
+  config.set({
 
-  conf.files = conf.files.concat([
-    'test/mocha.conf.js',
-    './test/controllerSpec.js'
-  ]);
-  logLevel: config.LOG_INFO,
-
-  
-  config.set(conf);  
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['browserify', 'mocha', 'chai'],
+    plugins: [
+      'karma-chrome-launcher',
+      'karma-browserify',
+      'karma-mocha',
+      'karma-chai',
+      'karma-phantomjs-launcher',
+    ],
+    // list of files / patterns to load in the browser
+    files: [
+      'build/bundle.js',
+      //Test-Specific Code
+      'node_modules/chai/chai.js',
+      'test/lib/chai-should.js',
+      'test/lib/chai-expect.js',
+      'test/mocha.conf.js',
+      'test/controllerSpec.js'
+    ],
+    // list of files to exclude
+    exclude: [
+    ],
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+      'test/controllerSpec.js': ['browserify']
+    },
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress'],
+    // web server port
+    port: 9876,
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['PhantomJS'],
+    browserify: {
+      watch: true,
+      debug: true
+    },
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false
+  })
 }
